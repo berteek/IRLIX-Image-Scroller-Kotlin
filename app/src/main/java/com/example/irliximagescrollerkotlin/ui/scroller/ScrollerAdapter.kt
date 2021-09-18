4a35b73503b1ea74a79e4c48993abd689044e594
berteek <merzinalbert@gmail.com>
2021-09-18 23:18:55 +0400

    Adapter and holder created

---
+++ app/src/main/java/com/example/irliximagescrollerkotlin/ui/scroller/ScrollerAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.irliximagescrollerkotlin.ui.scroller
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.irliximagescrollerkotlin.data.ImageBlock
+import com.example.irliximagescrollerkotlin.databinding.ImageBlockBinding
+
+class ScrollerAdapter: RecyclerView.Adapter<ScrollerAdapter.ImageBlockViewHolder>() {
+
+    private var imageBlocks: List<ImageBlock>? = emptyList()
+
+    class ImageBlockViewHolder(private val binding: ImageBlockBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageBlock: ImageBlock?) {
+            binding.apply {
+                Glide.with(cardView)
+                    .load(imageBlock?.imageURL)
+                    .into(imageView)
+                usernameView.text = imageBlock?.username
+                likesTextView.text = imageBlock?.likes.toString()
+                tagsView.text = imageBlock?.tags
+                Glide.with(cardView)
+                    .load(imageBlock?.userImageURL)
+                    .into(userImageView)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageBlockViewHolder {
+        return ImageBlockViewHolder(ImageBlockBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ImageBlockViewHolder, position: Int) {
+        val currentImageBlock = imageBlocks?.get(position)
+        holder.bind(currentImageBlock)
+    }
+
+    override fun getItemCount(): Int {
+        val nullableSize: Int? = imageBlocks?.size
+
+        return if (nullableSize != null) {
+            val size: Int = nullableSize
+            size
+        } else {
+            0
+        }
+    }
+
+    fun setImageBlocks(imageBlocks: List<ImageBlock>?) {
+        this.imageBlocks = imageBlocks
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/irliximagescrollerkotlin/ui/scroller/ScrollerFragment.kt
+++ app/src/main/java/com/example/irliximagescrollerkotlin/ui/scroller/ScrollerFragment.kt
@@ -6,17 +6,39 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.irliximagescrollerkotlin.R
 import com.example.irliximagescrollerkotlin.data.ImageBlock
+import com.example.irliximagescrollerkotlin.databinding.FragmentScrollerBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ScrollerFragment : Fragment(R.layout.fragment_scroller) {
 
     private val viewModel by viewModels<ScrollerViewModel>()
+
+    private var _binding: FragmentScrollerBinding? = null
+    private val binding get() = _binding!!
+
     private var imageBlocks: List<ImageBlock>? = emptyList()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _binding = FragmentScrollerBinding.bind(view)
+
+        val adapter = ScrollerAdapter()
+
+        binding.apply {
+            rcView.setHasFixedSize(true)
+            rcView.adapter = adapter
+        }
+
         imageBlocks = viewModel.getImageBlocks(viewLifecycleOwner)
+
+        adapter.setImageBlocks(imageBlocks)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
     }
 }
